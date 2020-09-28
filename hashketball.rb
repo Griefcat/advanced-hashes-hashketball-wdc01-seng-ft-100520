be2c6be09d2c5c1e68da5d2b181302fb819e7bf8
Anne Nardolilli <anardolilli@gmail.com>
2020-09-28 22:00:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -189,7 +189,7 @@ end
 def big_shoe_rebounds(shoe)
   game_hash.each do |team_side, team_info|
     team_info[:players].each do |player|
-    if player[:shoe_size] > shoe
+    if player[:shoe_size] >= shoe
       puts player[:rebounds]
     end 
   end 
