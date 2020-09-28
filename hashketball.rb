60c7c20491bd21a2c501215f9e6cd2408ec9803f
Anne Nardolilli <anardolilli@gmail.com>
2020-09-28 23:02:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -191,9 +191,9 @@ def big_shoe_rebounds
   number_of_rebounds = 0
   game_hash.each do |team_side, team_info|
      team_info[:players].each do |player|
-      binding.pry
        if player[:shoe] > largest_shoe_size 
        return player[:rebounds]
+       binding.pry
       end 
     end 
   end 
